9588781e0b56270ccddc6f3ff60a9ff2da865fb5
dkumagai <doug@mapletonhillmedia.com>
2016-06-10 10:30:45 -0600

    initial commit

---
+++ Dynamic/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Dynamic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            dynamic value = new SpecialDynamic();
+            var propName = "DynamicStuff";
+            value.Value = "value";
+            value.Stuff = "extra";
+            value[propName] = "Another Value";
+            var serialize = JsonConvert.SerializeObject(value);
+            var properties = value.GetDynamicMemberNames();
+            Console.WriteLine();
+            Console.WriteLine(serialize);
+        }
+    }
+}
---
+++ Dynamic/SpecialDynamic.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Dynamic
+{
+    public class SpecialDynamic : DynamicObject
+    {
+        private readonly Dictionary<string,object> _members = new Dictionary<string, object>(); 
+        [JsonProperty]
+        public string Value { get; set; }
+
+        public override bool TrySetMember(SetMemberBinder binder, object value)
+        {
+            _members.Add(binder.Name, value);
+            return true;
+        }
+
+        public override bool TryGetMember(GetMemberBinder binder, out object result)
+        {
+            result = _members[binder.Name];
+            return true;
+        }
+
+        public override bool TryGetIndex(GetIndexBinder binder, object[] indexes, out object result)
+        {
+            result = _members[indexes.First().ToString()];
+            return true;
+        }
+
+        public override bool TrySetIndex(SetIndexBinder binder, object[] indexes, object value)
+        {
+            _members[indexes.First().ToString()] = value;
+            return true;
+        }
+
+        public override IEnumerable<string> GetDynamicMemberNames()
+        {
+            return _members.Keys;
+        }
+    }
+}
